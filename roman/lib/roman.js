513ddbd2860a74fef002b688b9c11373696aa4a2
jaitone <jaitoone@gmail.com>
2021-02-24 15:30:52 +0000

    Added two more tests and solved

--- roman/lib/roman.js
+++ roman/lib/roman.js
@@ -1,7 +1,20 @@
 class RomanConverter {
+
+	constructor() {
+		this.numbers = [1, 4, 5, 9,	10,	40, 50,	90, 100, 400, 500, 900, 1000]
+		this.romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" ]
+	}
+
     
-    fromNumerToRoman(number) {
-			return "I"
+    fromNumberToRoman(number) {
+			let numberIndex;
+			this.numbers.forEach((n) => {
+				if(n === number) {
+					numberIndex = this.numbers.indexOf(number);
+				}
+				
+			})
+			return this.romans[numberIndex];
     }
     
 }
--- roman/spec/roman.test.js
+++ roman/spec/roman.test.js
@@ -8,22 +8,23 @@ describe("Convertor", () => {
     });
     
     it('converts 1 into I', () => {
-        expect(convertor.fromNumerToRoman(1)).toBe("I");
+        expect(convertor.fromNumberToRoman(1)).toBe("I");
     });
     
-    xit('converts 2 into II', () => {
-        expect(convertor.sum(1, 2)).toBe(3);
-    });
+    // it('converts 2 into II', () => {
+    //     expect(convertor.fromNumberToRoman(2)).toBe("II");
+    // });
+
+    // it('converts 3 into III', () => {
+    //     expect(convertor.fromNumberToRoman(2)).toBe("III");
+    // });
     
-    xit('converts 3 into III', () => {
-        expect(convertor.sum(1, 2)).toBe(3);
+    it('converts 4 into IV', () => {
+        expect(convertor.fromNumberToRoman(4)).toBe("IV");
     });
-    
-    xit('converts 4 into IV', () => {
-        expect(convertor.sum(1, 2)).toBe(3);
+
+    it('converts 5 into V', () => {
+        expect(convertor.fromNumberToRoman(5)).toBe("V");
     });
-    
-    xit('converts 5 into V', () => {
-        expect(convertor.sum(1, 2)).toBe(3);
-    });
+
 })
